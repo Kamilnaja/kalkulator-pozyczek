76da33776b23a3d355fb810cafa9d0978d6023a6
Kamil Naja <kamilnaja@gmail.com>
2017-03-14 20:13:18 +0100

    logika ukończona

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function count (value, time, procent) {
     //jeśli pożyczka to oblicz na początku
     var rate = sum / time;
     var returnOfInvestment = sum - value;
-    var output =
-    console.log('value = ' + value + ' rate = ' + rate + ' time = ' + time + ' sum = ' + sum + ' zwrot z inwestycji to ' + returnOfInvestment + 'procent = ' + procent);
+    var output = ('Kwota inwestycji to= ' + Math.round(value) + '<br> Wartość raty wynosi = ' + Math.round(rate) + '<br>Czas kredytu w miesiącach wynosi = ' + Math.round(time) + '<br>Całkowita kwota kredytu wynosi = ' + sum + '<br>Procent pożyczki wynosi = ' + Math.round(procent));
+  $('.sum').html(output);
+
 }
 $(document).ready(function(){
     //event listeners
@@ -14,10 +15,9 @@ $(document).ready(function(){
         var procent = $('#procent').val() * 1;
         var time = $('#time').val() * 1;
         count(value,  time, procent);
-        $('#userInput').text(value);
+
     });
 });
-//pożyczka
 
 
 
